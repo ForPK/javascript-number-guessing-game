5d3a1f83b630b6560a5b8384a10a33db20a91e57
ForPK <for_pk@naver.com>
2020-08-11 00:50:37 +0900

    code check

--- js.js
+++ js.js
@@ -1,33 +1,26 @@
 "use strict";
 
-let randomNum = Math.floor(Math.random() * 100) + 1;
 const numForm = document.querySelector("#game-wrap .form"),
   numInput = document.querySelector("#game-wrap .input"),
   numBtn = document.querySelector("#game-wrap .btn-save"),
   resultWrap = document.getElementById("result-wrap"),
   userValueList = document.querySelector(".user-answer"),
   result = document.querySelector("#result-wrap .result"),
-  resultNum = document.querySelector("#result-wrap .result-num"),
   correct = document.getElementById("correct"),
   wrong = document.getElementById("wrong"),
   over = document.getElementById("over"),
   lowNhigh = document.querySelector("#wrong .lowhigh"),
   chanceNum = document.querySelector("#wrong .chance"),
   answerNum = document.querySelector(".num-right"),
-  btnNewGame = document.getElementById("btn-new-game");
+  btnNewGame = document.getElementById("btn-new-game"),
+  numList = [];
 
-let userNum = document.querySelector("#result-wrap .user-answer .num"),
-  numList = [],
+let randomNum = Math.floor(Math.random() * 100) + 1,
+  userNum = document.querySelector("#result-wrap .user-answer .num"),
   countNum = 1;
 
-function startRandomNum() {
-  return randomNum;
-}
-
 function submitForm() {
-  if (numInput.value !== null) {
-    numForm.addEventListener("submit", numSubmit);
-  }
+  numForm.addEventListener("submit", numSubmit);
 }
 
 function numSubmit(e) {
@@ -51,7 +44,7 @@ function inputCheck() {
   }
 }
 
-function common() {
+function pushInputValue() {
   numList.push(numInput.value);
   userNum.innerText = numList.join(" ");
 }
@@ -80,7 +73,7 @@ function countDown() {
   }
 
   function numCorrect() {
-    common();
+    pushInputValue();
     wrong.classList.remove("disblock");
     correct.classList.add("disblock");
     btnNewGame.classList.add("disblock");
@@ -88,7 +81,7 @@ function countDown() {
   }
 
   function numWrong() {
-    common();
+    pushInputValue();
     chanceNum.innerText = `${10 - countNum}번 남았습니다.`;
     if (randomNum < inputValue) {
       lowNhigh.innerText = "입력한 숫자보다 낮게";
@@ -102,16 +95,20 @@ function countDown() {
   }
 
   function gameOver() {
+    pushInputValue();
     wrong.classList.remove("disblock");
     over.classList.add("disblock");
     btnNewGame.classList.add("disblock");
     answerNum.innerText = `정답은 ${randomNum}이였지롱~!!`;
-    common();
     numInput.value = " ";
     numInput.disabled = true;
     numBtn.disabled = true;
     newGame();
   }
+}
+
+function newGame() {
+  btnNewGame.addEventListener("click", resetGame);
 }
 
 function resetGame() {
@@ -130,13 +127,4 @@ function resetGame() {
   randomNum = Math.floor(Math.random() * 100) + 1;
 }
 
-function newGame() {
-  btnNewGame.addEventListener("click", resetGame);
-}
-
-function init() {
-  startRandomNum();
-  submitForm();
-}
-
-init();
+submitForm();
